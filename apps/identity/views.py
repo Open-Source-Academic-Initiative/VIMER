75a801da2befd7cdf1b06f2098c1335cc57413e2
aicruma <amurcia@protonmail.com>
2026-05-05 19:04:50 -0500

    Remove unused identity view import

--- apps/identity/views.py
+++ apps/identity/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views import View
 from django.views.generic import FormView, ListView, TemplateView
